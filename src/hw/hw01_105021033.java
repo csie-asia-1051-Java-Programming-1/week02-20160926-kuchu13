adc763c34dcfa37df6332cc451ad0ed3130892bf
kuchu13 <105021033@live.asia.edu.tw>
2016-09-29 23:46:30 +0800

    ING

--- src/hw/hw01_105021033.java
+++ src/hw/hw01_105021033.java
@@ -1,9 +1,36 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01_105021033 {
 
 	public static void main(String[] args) {
 
+//假設銅板有1元、5元、50元共三種，媽媽請小明去菜市場買水果，給了小明N元，
+//且媽媽交待，要老闆找小明的零錢的數目要最少，
+//而小明到了水果攤買了a1顆蘋果，a2顆柳丁，及a3顆桃子，1顆蘋果15元，1顆柳丁20元，1顆桃子30元，
+//請問老問需找多少個1元、5元、50元，其銅板數目最少。
+//(先輸入媽媽給小明多少錢，N，接著輸入a1, a2, a3 ， 在此n, a1, a2, a3為整數，
+//且a115+ a220+ a3*30小於或等於N。列出共找小明多少個1元，5元及50元，
+//若帶的錢不夠買水果，則顯示”0”)
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.println("媽媽給了多少元");
+		int n = scn.nextInt();	
+		int a1 = scn.nextInt();
+		int a2 = scn.nextInt();
+		int a3 = scn.nextInt();
+		System.out.println();
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
