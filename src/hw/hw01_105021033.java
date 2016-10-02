2aa9565c7ba24cc1fb5f5b819ad41132072ea624
kuchu13 <105021033@live.asia.edu.tw>
2016-10-02 20:40:32 +0800

    10.2Hw1 OK

--- src/hw/hw01_105021033.java
+++ src/hw/hw01_105021033.java
@@ -5,32 +5,23 @@ import java.util.Scanner;
 public class hw01_105021033 {
 
 	public static void main(String[] args) {
-
-//假設銅板有1元、5元、50元共三種，媽媽請小明去菜市場買水果，給了小明N元，
-//且媽媽交待，要老闆找小明的零錢的數目要最少，
-//而小明到了水果攤買了a1顆蘋果，a2顆柳丁，及a3顆桃子，1顆蘋果15元，1顆柳丁20元，1顆桃子30元，
-//請問老問需找多少個1元、5元、50元，其銅板數目最少。
-//(先輸入媽媽給小明多少錢，N，接著輸入a1, a2, a3 ， 在此n, a1, a2, a3為整數，
-//且a115+ a220+ a3*30小於或等於N。列出共找小明多少個1元，5元及50元，
-//若帶的錢不夠買水果，則顯示”0”)
-		
+	
 		Scanner scn = new Scanner(System.in);
 		System.out.println("媽媽給了多少元");
-		int n = scn.nextInt();	
+		int n = scn.nextInt();
+		System.out.println("買了幾顆蘋果、柳丁、桃子");
 		int a1 = scn.nextInt();
 		int a2 = scn.nextInt();
 		int a3 = scn.nextInt();
-		System.out.println();
+		
+		int a4 = (n-(a1*15+a2*20+a3*30))/50;
+		int a5 = ((n-(a1*15+a2*20+a3*30))-a4*50)/5;
+		int a6 = ((n-(a1*15+a2*20+a3*30))-a4*50-a5*5)/1;
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		if(n>=a1*15+a2*20+a3*30){
+			System.out.println("共找小明"+a6+"個1元，"+a5+"個5元及"+a4+"個50元");
+		}else{
+			System.out.println(0);
+		}
 	}
-
 }
